b273bd3f23475fd6744231c68d6d400b2b5697fd
William <benjotoure@gmail.com>
2023-01-27 09:46:52 +0100

    Questions 1 à 4

--- interaction.js
+++ interaction.js
@@ -3,11 +3,41 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.x_init=0;
+  this.x_fin=0;
+  this.y_init=0;
+  this.y_fin=0;
+  this.canvas=canvas;
+  this.interactor=interactor;
+  this.press=false;
 
 	// Developper les 3 fonctions gérant les événements
+  this.presser=function(evt){
+    this.press=true;
+    x_init=this.getMousePosition(this.canvas,evt).x;
+    y_init=this.getMousePosition(this.canvas,evt).y;
+  }.bind(this)
+  
+  this.relacher=function(evt){
+    this.press=false;
+    x_fin=this.getMousePosition(this.canvas,evt).x;
+    y_fin=this.getMousePosition(this.canvas,evt).y;
+  }.bind(this)
+
+  this.deplacer=function(evt){
+    if(this.press){
+      x_fin=this.getMousePosition(this.canvas,evt).x;
+      y_fin=this.getMousePosition(this.canvas,evt).y;
+    }
+   
+  }.bind(this)
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.presser, false);
+  canvas.addEventListener('mouseup', this.relacher, false);
+  canvas.addEventListener('mousemove', this.presser, false);
 };
+
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
